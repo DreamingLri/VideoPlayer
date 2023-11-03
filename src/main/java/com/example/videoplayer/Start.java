681d8bed522887a446346ae5950732ee3d12f294
DreamingLri <3507415455@qq.com>
2023-11-03 22:54:18 +0800

    set icon size final

---
+++ src/main/java/com/example/videoplayer/Start.java
@@ -0,0 +1,9 @@
+package com.example.videoplayer;
+
+import javafx.application.Application;
+
+public class Start {
+    public static void main(String[] args) {
+        Application.launch(VideoPlayerApplication.class);
+    }
+}
